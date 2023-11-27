ea6f220995528245d17300b45d00c5fcc526ccae
James Esteban <james.esteban08@gmail.com>
2023-11-27 14:48:16 +0800

    Completed Day 2

---
+++ backend/day-02/day-2.py
@@ -0,0 +1,7 @@
+first_num = float(input("Enter the first number: "))
+second_num = float(input("Enter the second number: "))
+third_num = float(input("Enter the third number: "))
+
+sum = first_num + second_num + third_num
+
+print(f"Sum: {sum}")
